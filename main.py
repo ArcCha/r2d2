585caee54708e38e90e162dd7fb48e6e28bb2f48
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2018-02-21 20:54:45 +0100

    Implement add subcommand

--- main.py
+++ main.py
@@ -17,6 +17,14 @@ def init(args):
     repo = git.Repo.init(r2d2_path)
 
 
+def add(args):
+    r2d2_path = pathlib.Path.home().joinpath('.r2d2')
+    file_path = pathlib.Path(args.file).resolve()
+    target_path = r2d2_path.joinpath(file_path.name)
+    file_path.rename(target_path)
+    file_path.symlink_to(target_path)
+
+
 def main():
     parser = argparse.ArgumentParser(prog='r2d2')
     subparsers = parser.add_subparsers()
@@ -24,6 +32,10 @@ def main():
     parser_init = subparsers.add_parser('init')
     parser_init.add_argument('-f', '--force', action='store_true')
     parser_init.set_defaults(func=init)
+
+    parser_add = subparsers.add_parser('add')
+    parser_add.add_argument('file')
+    parser_add.set_defaults(func=add)
 
     args = parser.parse_args()
     if hasattr(args, 'func'):
